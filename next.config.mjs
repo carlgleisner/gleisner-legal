b4721b68e3beaa9627bbc84fd8ed13789a7c4604
Carl Gleisner <carlgleisner@users.noreply.github.com>
2022-10-12 18:05:48 +0200

    Move X-Frame-Options to vercel.json.

--- next.config.mjs
+++ next.config.mjs
@@ -40,10 +40,6 @@ const securityHeaders = [
     value: 'on',
   },
   {
-    key: 'X-Frame-Options',
-    value: 'SAMEORIGIN',
-  },
-  {
     key: 'Strict-Transport-Security',
     value: 'max-age=31536000; includeSubDomains; preload',
   },
