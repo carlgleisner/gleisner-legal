9790352baa9986a8bc44d044f276a9c52a97bda7
Carl Gleisner <carlgleisner@users.noreply.github.com>
2022-11-06 06:16:56 +0100

    Update headers for planned hosting change.

--- next.config.mjs
+++ next.config.mjs
@@ -10,7 +10,7 @@ const securityHeaders = [
   {
     key: 'Permissions-Policy',
     value:
-      'accelerometer=(), ambient-light-sensor=(), autoplay=(), battery=(), camera=(), cross-origin-isolated=(self), display-capture=(), document-domain=(), encrypted-media=(), execution-while-not-rendered=(), execution-while-out-of-viewport=(), fullscreen=(self), geolocation=(), gyroscope=(), keyboard-map=(), magnetometer=(), microphone=(), midi=(), navigation-override=(), payment=(), picture-in-picture=(), publickey-credentials-get=(), screen-wake-lock=(), sync-xhr=(), usb=(), web-share=(), xr-spatial-tracking=()',
+      'accelerometer=(), autoplay=(), camera=(), cross-origin-isolated=(self), display-capture=(), document-domain=(), encrypted-media=(), fullscreen=(self), geolocation=(), gyroscope=(), keyboard-map=(), magnetometer=(), microphone=(), midi=(), payment=(), picture-in-picture=(), publickey-credentials-get=(), screen-wake-lock=(), sync-xhr=(), usb=(), xr-spatial-tracking=()',
   },
   {
     key: 'NEL',
@@ -30,11 +30,11 @@ const securityHeaders = [
     key: 'Cross-Origin-Opener-Policy-Report-Only',
     value: 'same-origin; report-to="default"',
   },
-  {
-    key: 'Content-Security-Policy-Report-Only',
-    value:
-      "default-src 'self'; font-src 'self'; img-src 'self'; script-src 'self'; style-src 'self'; report-uri https://fyhx63fm.uriports.com/reports/report; report-to default",
-  },
+  // {
+  //   key: 'Content-Security-Policy-Report-Only',
+  //   value:
+  //     "default-src 'self'; font-src 'self'; img-src 'self'; script-src 'self'; style-src 'self'; report-uri https://fyhx63fm.uriports.com/reports/report; report-to default",
+  // },
   {
     key: 'X-DNS-Prefetch-Control',
     value: 'on',
@@ -51,6 +51,18 @@ const securityHeaders = [
     key: 'Referrer-Policy',
     value: 'same-origin',
   },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN',
+  },
+  // {
+  //   key: 'Strict-Transport-Security',
+  //   value: 'max-age=31536000; includeSubDomains; preload',
+  // },
   // {
   //   key: 'Onion-Location',
   //   value:
