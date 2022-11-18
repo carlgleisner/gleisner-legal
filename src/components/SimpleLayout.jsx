dccfcb6b237ac8addc17f49fc46644cb0e640cb1
Carl Gleisner <carlgleisner@users.noreply.github.com>
2022-11-18 08:02:11 +0100

    Only output lead paragraph in SimpleLayout when set.

--- src/components/SimpleLayout.jsx
+++ src/components/SimpleLayout.jsx
@@ -7,9 +7,11 @@ export function SimpleLayout({ title, intro, children }) {
         <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
           {title}
         </h1>
-        <p className="mt-6 text-base text-zinc-700 dark:text-zinc-300">
-          {intro}
-        </p>
+        {intro && (
+          <p className="mt-6 text-base text-zinc-700 dark:text-zinc-300">
+            {intro}
+          </p>
+        )}
       </header>
       <div className="mt-16 sm:mt-20">{children}</div>
     </Container>
