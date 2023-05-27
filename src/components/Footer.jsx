78d30c6c271c664e8172782116c862ab4c09e861
Carl Gleisner <carlgleisner@users.noreply.github.com>
2023-05-27 05:00:43 +0200

    Take down projects area.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -17,13 +17,12 @@ export function Footer() {
   return (
     <footer className="mt-32 print:hidden">
       <Container.Outer>
-        <div className="border-t border-zinc-100 pt-10 pb-16 dark:border-zinc-700/40">
+        <div className="border-t border-zinc-100 pb-16 pt-10 dark:border-zinc-700/40">
           <Container.Inner>
             <div className="flex flex-col items-center justify-between gap-6 lg:flex-row">
               <div className="flex gap-6 text-sm font-medium text-zinc-800 dark:text-zinc-200">
                 <NavLink href="/om">Om</NavLink>
                 <NavLink href="/artiklar">Artiklar</NavLink>
-                <NavLink href="/projekt">Projekt</NavLink>
                 {/* <NavLink href="http://tv3pzgycq3cbfm5ibmow3ucryfvqvlhcp3pmfsroupfda5g6dnxu6xid.onion/">
                   Onion site 🧅
                 </NavLink> */}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -123,7 +123,6 @@ function MobileNavigation(props) {
               <ul className="-my-2 divide-y divide-zinc-100 text-base text-zinc-800 dark:divide-zinc-100/5 dark:text-zinc-300">
                 <MobileNavItem href="/om">Om</MobileNavItem>
                 <MobileNavItem href="/artiklar">Artiklar</MobileNavItem>
-                <MobileNavItem href="/projekt">Projekt</MobileNavItem>
               </ul>
             </nav>
           </Popover.Panel>
@@ -162,7 +161,6 @@ function DesktopNavigation(props) {
       <ul className="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
         <NavItem href="/om">Om</NavItem>
         <NavItem href="/artiklar">Artiklar</NavItem>
-        <NavItem href="/projekt">Projekt</NavItem>
       </ul>
     </nav>
   )
